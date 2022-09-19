dbb7f2b4827fc13d0524411c1a5a9949dbee661f
fygen <justicestranger@gmail.com>
2022-09-19 12:52:08 +0300

    feat: 2 more to finish

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const mongoose = require("mongoose");
 //We get Schema definition from mongoose Library
 const Schema = mongoose.Schema;
 //We need to connect the database by using this function which can also a local file like connect("mongodb://localhost:3000/test") if mongoDB installed on the device
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI2, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // For consoling out the ip and URL
 app.use((req, res, next) => {
@@ -28,19 +28,23 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: "false" }));
 
 //Creating a Schemas for a new Object of exr.
-const exerciseSchema = new Schema({
-    username: { type: String },
-    description: { type: String, required: true },
-    duration: { type: Number, required: true },
-    date: { type: Date, default: Date.now() },
-    _id: { type: String, required: true }
-})
+// const exerciseSchema = new Schema({
+//     description: { type: String, required: true },
+//     duration: { type: Number, required: true },
+//     date: { type: String, default: () => { return new Date().toDateString() } },
+// }, { versionKey: false, })
 const userSchema = new Schema({
     username: { type: String, required: true },
+    count: { type: Number, default: 0 },
+    log: [{
+        description: { type: String, required: true },
+        duration: { type: Number, required: true },
+        date: { type: String, default: () => { return new Date().toDateString() } },
+    }]
 })
 //We create the User Model by using mongoosejs library and we can make manipulations.
 let User = mongoose.model("User", userSchema);
-let Exercise = mongoose.model("Exercise", exerciseSchema);
+// let Exercise = mongoose.model("Exercise", exerciseSchema);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
@@ -50,7 +54,7 @@ app.get('/', (req, res) => {
 app.post("/api/users", (req, res, next) => {
     console.log(`req.body.username: '${req.body.username}'`)
     let userName = req.body.username === "" ? next("Username is empty") : req.body.username;
-    User.find({ username: userName }, (err, user) => {
+    User.find({ username: userName }, "_id username", (err, user) => {
         if (err) console.log(err);
         console.log("In the find:", user)
         if (user.length == 0) {
@@ -63,7 +67,6 @@ app.post("/api/users", (req, res, next) => {
                 }
                 else {
                     console.log("User created:", user);
-                    // res.json(user);
                     res.json({ "username": user.username, "_id": user._id });
                 }
             })
@@ -71,7 +74,7 @@ app.post("/api/users", (req, res, next) => {
         else if (typeof user !== "undefined") {
             user = user[0];
             console.log("User available:", user);
-            res.json({ "username": user.username, "_id": user._id });
+            res.json(user);
         }
         else {
             console.log("Error on find:", err);
@@ -81,7 +84,7 @@ app.post("/api/users", (req, res, next) => {
 })
 
 app.get("/api/users", (req, res, next) => {
-    User.find({}, (err, users) => {
+    User.find({}, "_id username __v", (err, users) => {
         if (err) console.log(err)
         console.log("users found:", users);
         if (users.length !== 0) {
@@ -94,30 +97,32 @@ app.get("/api/users", (req, res, next) => {
 })
 
 app.post("/api/users/:_id/exercises", (req, res, next) => {
-    console.log(req.params._id)
+    console.log(req.params._id, "req.body.date is:", req.body.date)
+    let dateFormat = typeof req.body.date === "undefined"? undefined :new Date(...req.body.date.split("-")).toDateString();
+    console.log(dateFormat);
     User.findById(req.params._id, (err, user) => {
         if (err) {
             console.log(err);
             next("err");
         };
-        if(!user){
+        if (!user) {
             next(user);
         }
         console.log("user availability:", user);
         if (typeof user !== "undefined") {
-            let userName = user.username;
-            const userEx = new Exercise({
-                _id: req.params._id,
-                username: userName,
-                date: req.body.date !== "" ? req.body.date : null,
+            let toDBdate = typeof req.body.date !== "undefined" ? dateFormat : new Date().toDateString();
+            let logid = user.count;
+            user.log.push({
+                date: toDBdate,
                 duration: req.body.duration,
                 description: req.body.description
             })
-            userEx.save((err) => {
-                if (err) console.log(err)
+            user.count = user.count + 1;
+            user.save((err) => {
+                if (err) console.log("When saving DB err occured:", err)
                 next(err);
             })
-            res.json(userEx);
+            res.json({ "_id": user._id, "username": user.username, "date": user.log[logid].date, "duration": user.log[logid].duration, "description": user.log[logid].description });
         }
         else {
             next("user is not created yet")
@@ -125,6 +130,16 @@ app.post("/api/users/:_id/exercises", (req, res, next) => {
     })
 })
 
+app.get("/api/users/:_id/logs", (req, res, next) => {
+    console.log("id:", req.params._id)
+    User.findById(req.params._id, "_id username count log.date log.duration log.description", (err, user)=>{
+        if (err) console.error(err)
+        if (!user) next(user);
+        if (typeof user !== "undefined"){
+            res.json(user);
+        }
+    })
+})
 
 // ADD ABOVE
 //When APP launched then listen port 3000 and INFORM the user where the port is used.
